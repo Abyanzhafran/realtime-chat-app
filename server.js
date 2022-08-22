2376df790c6bfcb059bdef46b7e54c8c27f399c9
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-08-23 06:19:51 +0700

    init commit - add concurrently socket.io nodemon

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(port, () => {
+  console.log(`server running at http://localhost:${port}/`);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/*.{html,css,js}",
+    "./tailwind.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
