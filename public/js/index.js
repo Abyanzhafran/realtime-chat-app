4a5fdf6028d41339b6c1e3836a81ffb9d00261de
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-08-24 22:07:39 +0700

    update - nodemon json extension - chat feature

--- public/js/index.js
+++ public/js/index.js
@@ -4,3 +4,23 @@ let sidebar = document.querySelector("#sidebar");
 btn.addEventListener("click", () => {
   sidebar.classList.toggle("-translate-x-full");
 });
+
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function (msg) {
+  var divItem = document.createElement('div');
+  divItem.className = 'bg-gray-400 w-fit max-w-auto mx-4 rounded-lg'
+  divItem.innerHTML = '<div class="pt-2 px-3"><span class="text-dark text-lg font-bold">' + msg + '</span><p class="text-dark font-normal pb-3" id="messages">' + msg + '</p></div>'
+  messages.appendChild(divItem);
+  window.scrollTo(0, document.body.scrollHeight);
+});
