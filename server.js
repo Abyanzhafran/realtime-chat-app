d2f7af57ead009915877712bfffd7313c8606524
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-08-26 19:01:02 +0700

    add ejs, change folder structure to ejs

--- server.js
+++ server.js
@@ -2,34 +2,24 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require("socket.io")(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+  res.render('index')
+})
 
 app.get('/chat', (req, res) => {
-  res.sendFile(__dirname + '/public/chat.html');
-});
-
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-//   socket.on('disconnect', () => {
-//     console.log('user disconnected');
-//   });
-// });
+  res.render('chat')
+})
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
 });
-
-// app.listen(port, () => {
-//   console.log(`server running at  http://localhost:${port}/`)
-// })
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
