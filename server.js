cac6c9e8a1ac6d2005af199244f173b8a73983b9
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-08-27 11:14:57 +0700

    add validation

--- server.js
+++ server.js
@@ -4,14 +4,30 @@ const http = require('http').Server(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3001;
 
+var users = {}
+var rooms = {}
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/chat', (req, res) => {
+  users = req.body.username
+  rooms = req.body.room
+  if (users !== '' || rooms !== '') {
+    res.redirect('/chat')
+  }
+  res.redirect('/')
+})
+
 app.get('/chat', (req, res) => {
+  if (users == '' || rooms == '') {
+    res.redirect('/')
+  }
   res.render('chat')
 })
 
